adf228f6ef07ede674ba530bbee98249f85fb6a2
Gamaliel Barron <gbarronmm@gmail.com>
2020-06-26 12:25:23 -0500

    registration route

---
+++ server/models/UserModel.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const emailValidator = require('email-validator');
+const bcrypt = require('bcryptjs');
+
+const SALT_ROUNDS = 12;
+
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        index: {unique: true},
+        minlength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        index: {unique: true},
+        validate: {
+            validator: emailValidator.validate,
+            message: props => `${props.value} is not a valid email address!`
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        index: {unique: true},
+        minlength: 8
+    }
+}, {
+    timestamps: true
+});
+
+UserSchema.pre('save', async function preSave(next) {
+    const user = this;
+
+    if(!user.isModified('password')) return next();
+
+    try{
+        const hash = await bcrypt.hash(user.password, SALT_ROUNDS);
+        user.password = hash;
+        return next()
+    }catch(err){
+        return next(err)
+    }
+})
+
+UserSchema.methods.comparePassword = async function comparePassword(userPassword){
+    return bcrypt.compare(userPassword, this.password)
+}
+
+module.exports = mongoose.model('User', UserSchema);
--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const UserModel = require('../../models/UserModel');
 
 const router = express.Router();
 
 module.exports = () => {
   router.get('/registration', (req, res) => res.render('users/registration', { success: req.query.success }));
+
+  router.post('/registration', async (req, res, next) => {
+    try{
+      const user = new UserModel({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      const savedUser = await user.save();
+
+      if(savedUser) return res.redirect('/users/registration?success=true');
+      return next(new Error('Failed to save user for unknown reasons'));
+    }catch(err){
+      return next(err)
+    }
+  })
 
   router.get('/account', (req, res) => res.render('users/account', { user: req.user }));
 
