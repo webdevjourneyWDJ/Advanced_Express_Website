dba139f6532097c3d6e8693d017fa33a199ef496
Gamaliel Barron <gbarronmm@gmail.com>
2020-07-14 16:03:52 -0500

    added ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,11 +11,11 @@ module.exports = {
   deploy: {
     "production" : {
         "user" : "ubuntu",
-        "host" : "54.161.66.106",
+        "host" : "52.70.39.2",
         "ref"  : "origin/master",
         "repo" : "https://github.com/webdevjourneyWDJ/Advanced_Express_Website.git",
         "path" : "/home/ubuntu/apps",
-        "pre-setup" : "apt-get install git",
+        "pre-setup" : "echo 'This is pre-setup command'",
         "pre-deploy-local" : "echo 'This is a local executed command'",
         "post-deploy" : "cp ../.env ./ && npm install && pm2 startOrRestart ecosystem.config.js --env production"
     },
