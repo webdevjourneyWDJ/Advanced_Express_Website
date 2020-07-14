d1ee5d127452ec43f1aa1d84c73124e73b6ac0f2
Gamaliel Barron <gbarronmm@gmail.com>
2020-07-14 15:48:07 -0500

    added ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,3 @@
-// The purpose of this file is covered in CH 05, Video 06
 module.exports = {
   apps: [
     {
@@ -12,16 +11,13 @@ module.exports = {
   deploy: {
     "production" : {
         "user" : "ubuntu",
-        // Multi host is possible, just by passing IPs/hostname as an array
         "host" : "54.161.66.106",
-        // Branch
         "ref"  : "origin/master",
-        // Git repository to clone
         "repo" : "https://github.com/webdevjourneyWDJ/Advanced_Express_Website.git",
-        // Path of the application on target servers
         "path" : "/home/ubuntu/apps",
-        // Commands to be executed on the server after the repo has been cloned
-        "post-deploy" : "cp ../.env ./ && npm install && pm2 startOrRestart ecosystem.config.js --env production",
+        "pre-setup" : "apt-get install git",
+        "pre-deploy-local" : "echo 'This is a local executed command'",
+        "post-deploy" : "cp ../.env ./ && npm install && pm2 startOrRestart ecosystem.config.js --env production"
     },
   },
 };
